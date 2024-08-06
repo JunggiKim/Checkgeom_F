f4d7744b36c6b725be354673f1e7c394aa125742
JunggiKim <k42ke@naver.com>
2024-08-06 15:11:42 +0900

    변수명 변경

--- src/core/component/AllSearchResults.jsx
+++ src/core/component/AllSearchResults.jsx
@@ -10,22 +10,24 @@ const AllSearchResults = React.memo(({AllLibraryBookData}) => {
             <p>검색된 결과가 없습니다.</p>
         )
     }
-    const bookDataList = AllLibraryBookData.libraryServiceResponseList
+    const bookDataList = AllLibraryBookData.librarySearchServiceResponseList
     console.log(bookDataList)
 
     return (
         <div className="allSearchResults-container">
-            {bookDataList.map((libraryData, index) =>
+            {bookDataList.map((libraryData) =>
                 (
                 <IndividualSearchResults
                     bookData={libraryData}
                 />
             ))
             }
+
         </div>
 
     );
 
 });
+
 
 export default AllSearchResults;
